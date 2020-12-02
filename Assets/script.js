8560894e2cb394502dd0194ce5030e760a751001
Russell Jewett <rfjewett3@gmail.com>
2020-12-02 17:35:37 -0500

    updates API data, begins logic for populating HTML.

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,74 @@
 
 var date = moment().format('MM/DD/YYYY');
 
-$('#btn').on('click', function() {
-    console.log('hello');
-    console.log(date);
-    console.log($('#input').val());
-})
+var cityNameEl = $('#cityName');
+var tempEl = $('#temp');
+var humidEl = $('#humidity');
+var windEl = $('#wind');
+var uvEl = $('#uv');
+
 
 // var populate = function(data) {
 
 // }
 
-var getWeather = function(city) {
-    var apiKey = '7719db6fa8f9f37f5cec7c50a9d6cc86';
-    var weatherURL = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-    var city = $('#input').val();
+var apiKey = '7719db6fa8f9f37f5cec7c50a9d6cc86';
+
+// create the function to fetch the weather at the current time.
+function getWeather(city) {
+    var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
     $.ajax({
         url: weatherURL,
-        method: 'GET'
-    }).then(function(response) {
-        populate(response);
+        method: 'GET',
+    }).then(function(weatherResponse) {
     })
 }
 
+// create the function to fetch the uv index for the location of the weather.
+function getUV(lon, lat) {
+    var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+    $.ajax({
+        url: uvURL,
+        method: 'GET'
+    }).then(function(uvResponse) {
+        console.log('UV: ', uvResponse);
+        console.log('UV Index: ', uvResponse.value);
+        return uvResponse;
+    })
+}
+
+// create the funciton to fetch the weather forecast for the next 5 days.
+function getForecast(city) {
+    var forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+    $.ajax({
+        url: forecastURL,
+        method: 'GET'
+    }).then(function(forecastResponse) {
+        // var fiveDayResponse = fiveDayForcast(response);
+        console.log('Forecast: ', forecastResponse);
+        return forecastResponse;
+    })
+}
+
+// logic for getting 5 day forecast
+function fiveDayForcast(response) {
+    var list = [];
+    for (i = 0; i < response.list.length; i += 8) {
+        list.append(i);
+    }
+}
+
+
+
+
+$('#btn').on('click', function() {
+    var city = $('#input').val();
+    console.log($('#input').val());
+    var weather = getWeather(city);
+    console.log(weather);
+    var uv = getUV(weather.coord.lon, weather.coord.lat);
+    console.log(uv);
+    var forecast = getForecast(city);
+    console.log(forecast);
+    cityNameEl.text(weather.name);
+})
